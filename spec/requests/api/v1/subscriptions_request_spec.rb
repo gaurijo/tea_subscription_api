47cdb7e755054d3dada372b9ec492ff9f247c84f
Gauri Joshi <gauri728@gmail.com>
2022-11-08 14:49:19 -0800

    Test and Sad Path Test: A subscription can be created for a Customer else 404 error

--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -6,7 +6,7 @@ describe "Subscriptions API" do
     lavender = Tea.create(title: "Lavender Haze", description: "Drift off into dreamland", temp: 123, brew_time: "3 min")
     green = Tea.create(title: "Green Tea", description: "Tranquility is one sip away", temp: 123, brew_time: "5 min")
     sub1 = Subscription.create(title: "Stress Buster", price: 15.25, status: "active", frequency: "monthly", customer_id: gauri.id, tea_id: lavender.id)
-    sub2 = Subscription.create(title: "Energizer", price: 20.10, status: "active", frequency: "monthly", customer_id: gauri.id, tea_id: green.id)
+    sub2 = Subscription.create(title: "Energizer", price: 20.10, status: 0, frequency: "monthly", customer_id: gauri.id, tea_id: green.id)
 
     get "/api/v1/customers/#{gauri.id}/subscriptions"
     # require 'pry'; binding.pry 
@@ -32,4 +32,68 @@ describe "Subscriptions API" do
       expect(sub[:attributes]).to include(:tea_id)
     end
   end
+
+    it "can create or subscribe a customer to a subscription" do 
+      gauri = Customer.create(first_name: "Gauri", last_name: "Jo", email: "gauri@gmail.com", address: "123 Test Ave, Denver, CO 12121")
+      maroon = Tea.create(title: "Maroon Hibiscus", description: "Floral and relaxing", temp: 123, brew_time: "3 min")
+      # new_sub = Subscription.create(title: "Sleepy Time", price: 10.15, status: "active", frequency: "weekly", customer_id: gauri.id, tea_id: maroon.id)
+      result = {title: "Sleepy Time", 
+                price: 10.15, 
+                status: "active", 
+                frequency: "weekly", 
+                customer_id: gauri.id,
+                tea_id: maroon.id }
+
+      headers = { "CONTENT_TYPE" => "application/json", "Accept" => "application/json"}
+
+      post "/api/v1/subscriptions", headers: headers, params: JSON.generate(result)
+      # require 'pry'; binding.pry 
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+      expect(result).to be_a(Hash)
+      expect(result).to have_key(:title)
+      expect(result).to have_key(:price)
+      expect(result).to have_key(:status)
+      expect(result).to have_key(:frequency)
+      expect(result).to have_key(:customer_id)
+      expect(result).to have_key(:tea_id)
+      expect(result[:title]).to eq("Sleepy Time")
+      expect(result[:price]).to eq(10.15)
+      expect(result[:status]).to eq("active")
+      expect(result[:frequency]).to eq("weekly")
+      expect(result[:customer_id]).to eq(gauri.id)
+      expect(result[:tea_id]).to eq(maroon.id)
+    end
+
+    it "renders a status 404 error if bad data is passed through" do 
+      gauri = Customer.create(first_name: "Gauri", last_name: "Jo", email: "gauri@gmail.com", address: "123 Test Ave, Denver, CO 12121")
+      maroon = Tea.create(title: "Maroon Hibiscus", description: "Floral and relaxing", temp: 123, brew_time: "3 min")
+
+      bad_result = {title: "Another Sub", 
+                    price: 10.15, 
+                    status: "active", 
+                    frequency: "weekly", 
+                    customer_id: 5, 
+                    tea_id: maroon.id}
+
+      headers = { "CONTENT_TYPE" => "application/json", "Accept" => "application/json"}
+
+      post "/api/v1/subscriptions", headers: headers, params: JSON.generate(bad_result)
+
+      expect(response).to_not be_successful 
+      expect(response.status).to eq(404)
+    end
+  # xit "can delete a customer's subscription" do 
+  #   gauri = Customer.create(first_name: "Gauri", last_name: "Jo", email: "gauri@gmail.com", address: "123 Test Ave, Denver, CO 12121")
+  #   lavender = Tea.create(title: "Lavender Haze", description: "Drift off into dreamland", temp: 123, brew_time: "3 min")
+  #   green = Tea.create(title: "Green Tea", description: "Tranquility is one sip away", temp: 123, brew_time: "5 min")
+  #   sub1 = Subscription.create(title: "Stress Buster", price: 15.25, status: "active", frequency: "monthly", customer_id: gauri.id, tea_id: lavender.id)
+  #   sub2 = Subscription.create(title: "Energizer", price: 20.10, status: 0, frequency: "monthly", customer_id: gauri.id, tea_id: green.id)
+
+  #   delete "/api/v1/customers/#{gauri.id}/subscriptions/#{sub2.id}"
+  #   require 'pry'; binding.pry 
+  #   subs = JSON.parse(response.body, symbolize_names: true)
+
+  #   expect(subs.count).to eq(1)
+  # end
 end
