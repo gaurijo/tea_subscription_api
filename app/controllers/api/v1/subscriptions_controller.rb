ef17e49275d40f28ad22ff2d3147e8363cf7f369
Gauri Joshi <gauri728@gmail.com>
2022-11-08 14:50:39 -0800

    Feature: Subscription create action will render new subscription else error status

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -3,4 +3,22 @@ class Api::V1::SubscriptionsController < ApplicationController
     customer = Customer.find(params[:id])
     render json: SubscriptionSerializer.new(customer.subscriptions)
   end
+
+  def create 
+    sub = Subscription.new(sub_params)
+    # if sub.save
+    if Customer.exists?(params[:customer_id]) && Tea.exists?(params[:tea_id])
+      # if Customer.find(params[:customer_id]) && Tea.find(params[:tea_id])
+      render json: SubscriptionSerializer.new(sub), status: :created
+    else 
+      render status: 404
+    end
+  end
+  # def destroy
+  #   subscription = Subscription.find(params[:id], params[:customer_id])
+  # end
+private 
+  def sub_params 
+    params.permit(:title, :price, :frequency, :status, :customer_id, :tea_id)
+  end
 end
