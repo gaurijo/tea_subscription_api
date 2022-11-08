dc600e1643f7cd4ee443a0baa3a0f89716b5e4ec
Gauri Joshi <gauri728@gmail.com>
2022-11-08 14:49:54 -0800

    update seed to destroy all before re-seeding, and restful routes for POST request

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       get '/customers/:id/subscriptions', to: 'subscriptions#index'
+      post '/subscriptions', to: 'subscriptions#create'
+      # delete '/customers/:id/subscriptions/:id', to: 'subscriptions#destroy'
     end
   end
 end
