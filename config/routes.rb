bbe06d8f1884104e49bcb7f53acb277bd8c0ba8c
Gauri Joshi <gauri728@gmail.com>
2022-11-08 18:18:19 -0800

    Removing subscription - PATCH request instead of DELETE

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       get '/customers/:id/subscriptions', to: 'subscriptions#index'
-      patch '/customers/:id/subscriptions', to: 'subscriptions#update'
       post '/subscriptions', to: 'subscriptions#create'
+      patch '/subscriptions/remove', to: 'subscriptions#update'
     end
   end
 end
